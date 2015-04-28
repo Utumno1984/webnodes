15cade30e28e530eb166c566bca8cf328c8985f0
Daniele Ingusci <shadowstronghold@msn.com>
2015-04-28 11:10:49 +0200

    to resolve conflict

---
+++ app.js
@@ -0,0 +1,44 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url') ;
+var config = JSON.parse(fs.readFileSync('./config/serverAddress.json', 'utf-8'));
+var server = http.createServer(onresponse).listen(config.app.server.port, config.app.server.address);
+console.log('server run at ' + config.app.server.address + ':' + config.app.server.port);
+
+
+
+console.log(config);
+//1st callback on success
+function onresponse(req, res){
+	var path = url.parse(req.url).pathname;
+	console.log('this is the pathname -> ' + path );
+
+	switch(path){
+		case '/':
+			res.writeHead(200, {'Content-type' : 'text/plain'});
+			res.end('success with pathname -> ' + path + '\n');
+		break;
+		case '/library':
+			fs.readFile('./library/book.txt', function(err, content){
+				onResource(err, content, res);
+			});
+		break;
+		default:
+			pageNotFound(res);
+	}
+}
+
+function pageNotFound(res){
+	res.writeHead(404);
+	res.end('404 - Not Found');
+}
+
+function onResource(err, content, res){
+	if(err){
+		res.writeHead(500);
+		return res.end('Cannot read file');
+	}
+
+	res.writeHead(200);
+	return res.end(content);
+}
